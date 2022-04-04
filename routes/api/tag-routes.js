038bd9b7dd11609202220f730ca68700d3131f6c
Apjuve <abiponce3@hotmail.com>
2022-04-03 20:54:15 -0400

    added comments to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,36 @@
+// Use route in order to save all routes in file
 const router = require('express').Router();
+// import models needed for routes in tags
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+// route to get all tags in database
 router.get('/', async(req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  
+  // Try/catch used in order to catch errors in promises that are using "await".
   try {
+    // Create variable that contains all tags using the findAll method in the sequelize library
     const tagsData = await Tag.findAll({
+      // Use include in order to return Products that apply to tag through the ProductTag model
       include: [{model: Product, through: ProductTag, as: 'taggedProducts'}]
     });
+    // If there are no errors return a status of 200 and the tags data in json format
     res.status(200).json(tagsData);
   } catch (err) {
+     // If there are any errors they will be in the catch, a 500 status code is returned aswell as the error
     res.status(500).json(err);
   }
 });
 
+// find a single tag by its `id`
 router.get('/:id', async(req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  
   try {
+    // Using the findByPk method in the sequelize library return a single tag based on its id value using the req.params.id to define that id value
     const singleTagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product, through: ProductTag, as: 'taggedProducts'}]
     });
+    // If there is no tag with the id specified in the req.params.id return a 404 status code and a json format message
     if (!singlgeTagData) {
       res.status(404).json({ message: 'No tag found with this id.'});
       return;
@@ -34,9 +42,10 @@ router.get('/:id', async(req, res) => {
   }
 });
 
+// route to create a new tag
 router.post('/', async(req, res) => {
-  // create a new tag
   try {
+    // Use the create method in the sequelize library and defining the body of the new tag as the req.body
     const newTag = await Tag.create(req.body);
     res.status(201).json(newTag);
   } catch (err) {
@@ -44,15 +53,18 @@ router.post('/', async(req, res) => {
   }
 });
 
+// route to update a single tag's name by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
   try {
+    // Using the update method in the sequelize library define which fields can be updated
     const updatedTag = await Tag.update(
       {
         tag_name: req.body.tag_name
       },
       {
+        // A where clause is given in order to define which specific tag to update
         where: {
+          // The where is set to the req.params.id
           id: req.params.id
         }
       }
@@ -63,9 +75,10 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+//route to delete on tag by its `id` value
 router.delete('/:id', async(req, res) => {
-  // delete on tag by its `id` value
   try {
+    // Similar to the update method, set a where clause to define which tag to delete using the destroy method in the sequelize library
     const deletedTag = await Tag.destroy({
       where: {
         id: req.params.id
@@ -81,4 +94,5 @@ router.delete('/:id', async(req, res) => {
   }
 });
 
+// Export the router so all the routes can be used in the index
 module.exports = router;
